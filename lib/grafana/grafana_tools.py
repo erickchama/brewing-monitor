06c9f1720e76c5fdbce81b842b9beaa1bea3b22e
Erick Chama <eachama@tenaris.com>
2023-12-27 19:34:16 -0600

    workin on grafana automatic dashboard

--- lib/grafana/grafana_tools.py
+++ lib/grafana/grafana_tools.py
@@ -1,9 +1,13 @@
 import requests
 import json
 
+import sys
+sys.path.insert(1, '/home/pi/iot-brewing/lib/influxdb')
+
+import influxdb_tools as db
+
 from loguru import logger
 logger.level("GRAFANA", no=50, color="<green>")
-
 
 def send_dashboard_to_grafana(dashboard):
     try:
@@ -20,26 +24,35 @@ def send_dashboard_to_grafana(dashboard):
         logger.log('GRAFANA','ERROR:'.format(e))
     return
 
-def generate_dashboard(measurement_name,recipe_panel):
-    with open('/home/pi/tilt_IoT_v2/base_dashboard.json','r') as f:
-        dashboard = json.load(f)
-    rep_query = """SELECT last(\"init_ph\"), last(\"final_ph\"), last(\"original_g\"), last(\"t_gravity\"), last(\"t_volume\"), last(\"a_volume\"), last(\"t_eff\"), last(\"a_eff\") FROM \"{}\"""".format(measurement_name)
-    for panel in dashboard["panels"]:
-        if panel['title'] == 'Recipe':
-            panel['options']['content'] = recipe_panel
-        if 'fieldConfig' in panel.keys():
-            if 'overrides' in panel['fieldConfig']:
-                if panel['fieldConfig']['overrides'] != []:
-                    if 'Time' in panel['fieldConfig']['overrides'][0]['matcher']['options']:
-                        pass
-                    else:
-                        panel['fieldConfig']['overrides'][0]['matcher']['options'] = measurement_name + '.last'
-        if 'targets' in panel.keys():
-            if 'measurement' in panel["targets"][0]:
-                panel["targets"][0]["measurement"] = measurement_name
-    for target in dashboard["panels"][0]["targets"]:
-        if 'query' in target.keys():
-            target["query"] = rep_query
-        target["measurement"] = measurement_name
-    dashboard["title"] = measurement_name
+# def generate_dashboard(measurement_name,recipe_panel):
+#     with open('/home/pi/iot-brewing/lib/grafana/base_dashboard.json','r') as f:
+#         dashboard = json.load(f)
+#     rep_query = """SELECT last(\"init_ph\"), last(\"final_ph\"), last(\"original_g\"), last(\"t_gravity\"), last(\"t_volume\"), last(\"a_volume\"), last(\"t_eff\"), last(\"a_eff\") FROM \"{}\"""".format(measurement_name)
+#     for panel in dashboard["panels"]:
+#         if panel['title'] == 'Recipe':
+#             panel['options']['content'] = recipe_panel
+#         if 'fieldConfig' in panel.keys():
+#             if 'overrides' in panel['fieldConfig']:
+#                 if panel['fieldConfig']['overrides'] != []:
+#                     if 'Time' in panel['fieldConfig']['overrides'][0]['matcher']['options']:
+#                         pass
+#                     else:
+#                         panel['fieldConfig']['overrides'][0]['matcher']['options'] = measurement_name + '.last'
+#         if 'targets' in panel.keys():
+#             if 'measurement' in panel["targets"][0]:
+#                 panel["targets"][0]["measurement"] = measurement_name
+#     for target in dashboard["panels"][0]["targets"]:
+#         if 'query' in target.keys():
+#             target["query"] = rep_query
+#         target["measurement"] = measurement_name
+#     dashboard["title"] = measurement_name
+#     return dashboard
+
+def generate_dashboard(batch_results,batch_name,influx_client):
+    db.write_data(influx_client,batch_results)
+    with open('base_dashboard.txt', 'r') as file: 
+        data = file.read() 
+        data = data.replace('ferm_control_test', batch_name)
+    dashboard = json.loads(data)
+    logger.log('GRAFANA',dashboard)
     return dashboard
--- main.py
+++ main.py
@@ -18,45 +18,6 @@ import time
 from loguru import logger
 logger.add("/home/pi/iot-brewing/logs/logger.log", compression="zip" , rotation="23:59", enqueue=True)
 
-def send_dashboard_to_grafana(dashboard):
-    try:
-        headers =  {"Accept": "application/json",
-                   "Content-Type": "application/json",
-                   "Authorization": "Bearer eyJrIjoiM00yUVpibkJINkpoNmRtemFyaktvTTRwZEtlaWpveE8iLCJuIjoicHl0aG9uIiwiaWQiOjF9"
-                   }
-        server= "http://localhost:3000"
-        url = server + "/api/dashboards/db"
-        payload = {"dashboard": dashboard}
-        p = requests.post(url, headers=headers, json=payload)
-        logger.debug(p)
-    except Excetion as e:
-        logger.debug(e)
-    return
-
-def generate_dashboard(measurement_name,recipe_panel):
-    with open('/home/pi/tilt_IoT_v2/base_dashboard.json','r') as f:
-        dashboard = json.load(f)
-    rep_query = """SELECT last(\"init_ph\"), last(\"final_ph\"), last(\"original_g\"), last(\"t_gravity\"), last(\"t_volume\"), last(\"a_volume\"), last(\"t_eff\"), last(\"a_eff\") FROM \"{}\"""".format(measurement_name)
-    for panel in dashboard["panels"]:
-        if panel['title'] == 'Recipe':
-            panel['options']['content'] = recipe_panel
-        if 'fieldConfig' in panel.keys():
-            if 'overrides' in panel['fieldConfig']:
-                if panel['fieldConfig']['overrides'] != []:
-                    if 'Time' in panel['fieldConfig']['overrides'][0]['matcher']['options']:
-                        pass
-                    else:
-                        panel['fieldConfig']['overrides'][0]['matcher']['options'] = measurement_name + '.last'
-        if 'targets' in panel.keys():
-            if 'measurement' in panel["targets"][0]:
-                panel["targets"][0]["measurement"] = measurement_name
-    for target in dashboard["panels"][0]["targets"]:
-        if 'query' in target.keys():
-            target["query"] = rep_query
-        target["measurement"] = measurement_name
-    dashboard["title"] = measurement_name
-    return dashboard
-
 def on_start():
     ready = False
     while 1:
@@ -101,6 +62,6 @@ if __name__ == "__main__":
     logger.debug('Searching for last batch in Brewfather')
     batch_results,batch_name = bf.get_last_batch_results()
     logger.debug('Generating or updating last batch dashboard')
-    #db.write_data(influx_client,batch_results)
+    graf.generate_dashboard(batch_results,batch_name,influx_client)
     if mode == 'TILT':
         main()
