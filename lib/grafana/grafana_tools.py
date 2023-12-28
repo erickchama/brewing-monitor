cde6f9e9bfc9ad11ceb7add1c44ce8b1c5afb3de
Erick Chama <eachama@tenaris.com>
2023-12-27 21:15:48 -0600

    typo error

--- lib/grafana/grafana_tools.py
+++ lib/grafana/grafana_tools.py
@@ -44,6 +44,6 @@ def generate_dashboard(batch_results,batch_name,influx_client):
     for panel in dashboard["panels"]:
         if panel.get('title') == 'Recipe':
             panel['options']['content'] = recipe_panel
-    dashboard = json.dumos(dashboard)
+    dashboard = json.dumps(dashboard)
     send_dashboard_to_grafana(dashboard)
     return
